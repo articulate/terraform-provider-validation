f6f6eebe3e25dd9646344a66cbeb07d846aad000
Matt Loberg <mloberg@articulate.com>
2022-03-14 09:27:58 -0500

    refactor(debug): use debug serve option

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"log"
 
 	"github.com/articulate/terraform-provider-validation/internal/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -21,15 +19,9 @@ func main() {
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/articulate/validation", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
-	}
-
-	plugin.Serve(opts)
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: provider.New(version),
+		ProviderAddr: "registry.terraform.io/articulate/validation",
+		Debug:        debugMode,
+	})
 }
